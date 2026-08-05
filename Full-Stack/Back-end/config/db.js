1ec07243e551175ab8169f49d0d157bdd786532d
elisabeth1047 <elisabethmargareta02@gmail.com>
2026-08-05 20:25:53 +0700

    Use Railway PostgreSQL

--- Full-Stack/Back-end/config/db.js
+++ Full-Stack/Back-end/config/db.js
@@ -1,22 +1,21 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
+import pg from "pg";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 const { Pool } = pg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'sidias',
-  password: process.env.DB_PASSWORD || '12345',
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.on('connect', () => {
-  console.log('Connected to the PostgreSQL database');
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL");
 });
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
 });
